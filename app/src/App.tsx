cc2d8e60597acc2b492a3d66f1f0a8b640a1d81a
david1alvarez <dalvarez@predictiveindex.com>
2019-10-12 16:28:54 -0400

    set up basic routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const App: React.FC = () => {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Home</p>
     </div>
   );
+};
+
+const About = () => {
+  return (
+    <div>
+      <p>About</p>
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <p>Contact</p>
+    </div>
+  );
+};
+class App extends Component {
+  render() {
+    return (
+        <Router>
+              <div>
+              <h1>W3Adda - Simple SPA</h1>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                      <Link to="/contact">Users</Link>
+                    </li>
+                  </ul>
+                </nav>
+
+                <Route path="/" exact component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+              </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
