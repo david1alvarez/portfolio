b0f9987ea83dfc2a43b4cf874c37a6a01e0b12d6
jimmycarney <jimmycarneytufts@gmail.com>
2019-10-26 16:45:09 -0400

    API Buttons
    Added buttons to make various dnd api calls

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,26 +6,6 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import DnDDashboardContent from './dnd/home';
 
 const App: React.FC = () => {
-  const http = require('https');
-  function test() { 
-    http.get('https://api.open5e.com/classes/', (resp:any) => {
-      let data = '';
-
-      // A chunk of data has been recieved.
-      resp.on('data', (chunk:any) => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Print out the result.
-      resp.on('end', () => {
-        alert(data);
-      });
-
-    }).on("error", (err:any) => {
-      console.log("Error: " + err.message);
-    });
-  }
-  
   return (
     <div className="App">
       <header className="App-header">
@@ -33,7 +13,6 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <button onClick={ test }>Jimmy's Test</button>
         <a
           className="App-link"
           href="https://reactjs.org"
--- app/src/dnd/home.tsx
+++ app/src/dnd/home.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import http from 'https';
+import { any } from 'prop-types';
 
 interface Props {
     title: string;
@@ -15,12 +17,35 @@ export default class DnDApp extends Component<Props, State> {
     };
 
     changeStatus = () => {
-        this.setState({displayedBoolean: (!this.state.displayedBoolean)})
+        this.setState({displayedBoolean: (!this.state.displayedBoolean)});
+    }
+
+    test(key:string):any { 
+        http.get('https://api.open5e.com/' + key, (resp:any) => {
+          let data = '';
+    
+          // A chunk of data has been recieved.
+          resp.on('data', (chunk:any) => {
+            data += chunk;
+          });
+    
+          // The whole response has been received. Print out the result.
+          resp.on('end', () => {
+            alert(data);
+          });
+    
+        }).on("error", (err:any) => {
+          console.log("Error: " + err.message);
+        });
     }
 
     render() {
         return(
             <div>
+                <button onClick={this.test("classes")}>Classes</button>
+                <button onClick={this.test("races")}>Races</button>
+                <button onClick={this.test("monsters")}>Monsters</button>
+                <button onClick={this.test("weapons")}>Weapons</button>
                 <div>
                     {this.props.title}
                 </div>
